234526f88e7535e6af234834f6da68ddcbe12503
MK Frechette <mollykatefrechette@gmail.com>
2022-10-02 14:56:22 -0400

    made changes to ejs files / header partial

--- routes/main.js
+++ routes/main.js
@@ -3,34 +3,42 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const stylistController = require('../controllers/stylist');
+const modelController = require('../controllers/model');
 const { ensureAuth, ensureGuest, ensureAuthModel, ensureAuthStylist } = require("../middleware/auth");
 
-//Main Routes - simplified for now
+//Main Route
 router.get("/", homeController.getIndex);
+
+//Profile Routes
 router.get("/profile/model", ensureAuthModel, postsController.getProfile);
 router.get("/profile/stylist", ensureAuthStylist, postsController.getProfile)
-//router.get("/profile", passport.authenticate, postsController.getProfile);
+
+//Feed Routes
 router.get("/feed", ensureAuth, postsController.getFeed);
+
+//Login/Logout Routes
 router.get("/login", authController.getLogin);
-//router.post("/login", authController.postLogin);
-router.get("/logout", authController.logout);
-router.get("/signup", authController.getSignup);
-/*router.post("/signup", authController.postSignup);*/
-
-
-router.post("/signup/stylist", authController.postStylistSignup);
-
-
-router.post("/signup/model", authController.postModelSignup);
-
 router.post('/login/model', authController.postLoginModel);
 router.post('/login/stylist', authController.postLoginStylist);
-//Models Routes
+router.get("/logout", authController.logout);
 
+//Signup Routes
+router.post("/signup/stylist", authController.postStylistSignup);
+router.post("/signup/model", authController.postModelSignup);
+router.get("/signup", authController.getSignup);
+
+//Models Routes
+router.get("/models", modelController.getModels)
 
 //Stylist Routes
+router.get("/stylists", stylistController.getStylists)
 
-//Auth Routes
+//Old Routes
+
+//router.post("/login", authController.postLogin);
+/*router.post("/signup", authController.postSignup);*/
+//router.get("/profile", passport.authenticate, postsController.getProfile);
 
 
 module.exports = router;
