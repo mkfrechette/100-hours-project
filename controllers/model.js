1d4ebb3021c711389f264b23a3de8a42084c296e
MK Frechette <mollykatefrechette@gmail.com>
2022-11-12 14:02:47 -0500

    adding changes

--- controllers/model.js
+++ controllers/model.js
@@ -6,8 +6,17 @@ module.exports = {
   getModels: async (req, res) => {
     try {
       const models = await Model.find().sort({ createdAt: "desc" }).lean();
-      const proPic = await ProPic.find({ user: req.user.id });
-      res.render("models.ejs", { models: models, proPic: proPic });
+      const modelId = models.map((e) => String(e._id));
+      const proPic = await ProPic.find({ user: { $in: modelId } });
+      const modelProfile = proPic.reduce((a, c) => ({ ...a, [c._id]: c }), {});
+      console.log(modelProfile);
+
+      res.render("models.ejs", {
+        models: models,
+        proPic: proPic,
+        modelId: modelId,
+        modelProfile: modelProfile,
+      });
     } catch (err) {
       console.log(err);
     }
