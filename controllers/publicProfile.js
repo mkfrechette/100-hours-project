8c1a2a6e4cd9aeba5b2831bb6784380ca971d41c
MK Frechette <mollykatefrechette@gmail.com>
2022-11-20 13:35:17 -0500

    changes to public profiles

--- controllers/publicProfile.js
+++ controllers/publicProfile.js
@@ -4,15 +4,28 @@ const Model = require("../models/ModelSchema");
 const Post = require("../models/Post");
 
 module.exports = {
-    getPublicProfile: async (req, res) => {
-      try {
-        const user = await Post.find({ user: req.user.id }).sort({ createdAt: "desc" }).lean(); // posts of logged in user
-        const posts = await Post.find({ user: req.params.userId }).populate('user').sort({ createdAt: "desc" }).lean(); // posts by userId
-        const getUserInfo = await Model.findById(req.params.userId) // finds user info from userId in params
-        const username = getUserInfo.userName // gets username from user object
-        res.render("profile.ejs", { posts, user: req.user, username: username })
-      } catch (err) {
-        console.log(err);
+  getPublicProfile: async (req, res) => {
+    try {
+      const params = req.params.userId
+      console.log(params)
+      const model = await Model.findOne({ userName:  params }).lean(); 
+      const stylist = await Stylist.findOne({ userName: params }).lean();
+  
+      if(!model) {
+          const posts = await Post.find({ user: stylist._id }).lean();
+          const profile = stylist;
+          const proPic = await ProPic.findOne({ user: profile._id }).lean();
+          console.log(proPic)
+          res.render("pubProfile.ejs", { posts, profile, proPic });
+      }  else {
+          const posts = await Post.find({ user: model._id }).lean();
+          const profile = model;  
+          const proPic = await ProPic.findOne({ user: profile._id }).lean();
+          console.log(proPic)
+          res.render("pubProfile.ejs", { posts, profile, proPic });
       }
-    },
+    } catch (err) {
+      console.log(err);
+    }
+  },
 }
